b3f686ccc1e33cfe15eccbda3b6fdd1a19d960db
koviatsinets <koviatsinets@gmail.com>
2022-07-09 03:05:06 +0300

    add: js-filter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { doc } from 'prettier';
 import './global.css';
 
 // ------------------ Организация массива данных для товаров ------------------ //
@@ -70,45 +71,162 @@ const productsArr = [
         "img": "./assets/images/hp-pavilion-8-512.jpeg"
     },
     {
-     
+        "name": "HP ProBook 14\" 16Gb 256Gb",
+        "brand": "HP",
+        "display": "14",
+        "ram": "16",
+        "storage": "256",
+        "color": "silver",
+        "price": "1090",
+        "year": "2021",
+        "img": "./assets/images/hp-probook-16-256.jpg"
     },
     {
-      
+        "name": "HP EliteBook 16\" 16Gb 1Tb",
+        "brand": "HP",
+        "display": "16",
+        "ram": "16",
+        "storage": "1Tb",
+        "color": "silver",
+        "price": "1700",
+        "year": "2022",
+        "img": "./assets/images/hp-elitebook-16-512.jpg"
     },
     {
-
+        "name": "ASUS ZenBook 13\" 8Gb 256Gb",
+        "brand": "ASUS",
+        "display": "13",
+        "ram": "8",
+        "storage": "256",
+        "color": "black",
+        "price": "890",
+        "year": "2021",
+        "img": "./assets/images/asus-zenbook-8-256.jpg"
     },
     {
-
+        "name": "ASUS VivoBook 15\" 8Gb 256Gb",
+        "brand": "ASUS",
+        "display": "15",
+        "ram": "8",
+        "storage": "256",
+        "color": "grey",
+        "price": "700",
+        "year": "2020",
+        "img": "./assets/images/asus-vivobook-8-256.jpg"
     },
     {
-      
+        "name": "ASUS RogStrix 16\" 16Gb 1Tb",
+        "brand": "ASUS",
+        "display": "16",
+        "ram": "16",
+        "storage": "1024",
+        "color": "black",
+        "price": "1800",
+        "year": "2022",
+        "img": "./assets/images/asus-rogstrix-16-1Tb.webp"
     },
     {
-      
+        "name": "ASUS Tuf Gaming Dash 16\" 16Gb 512Gb",
+        "brand": "ASUS",
+        "display": "16",
+        "ram": "16",
+        "storage": "512",
+        "color": "black",
+        "price": "1590",
+        "year": "2022",
+        "img": "./assets/images/asus-tuf-16-512.webp"
     },
     {
-      
+        "name": "Lenovo Legion 16\" 16Gb 512Gb",
+        "brand": "Lenovo",
+        "display": "16",
+        "ram": "16",
+        "storage": "512",
+        "color": "grey",
+        "price": "2050",
+        "year": "2022",
+        "img": "./assets/images/lenovo-legion-16-1024.jpg"
     },
     {
-     
+        "name": "Lenovo Ideapad 15\" 8Gb 256Gb",
+        "brand": "Lenovo",
+        "display": "15",
+        "ram": "8",
+        "storage": "256",
+        "color": "silver",
+        "price": "880",
+        "year": "2020",
+        "img": "./assets/images/lenovo-ideapad-8-256.jpg"
     },
     {
-     
+        "name": "Lenovo V14 14\" 8Gb 256Gb",
+        "brand": "Lenovo",
+        "display": "14",
+        "ram": "8",
+        "storage": "256",
+        "color": "black",
+        "price": "680",
+        "year": "2020",
+        "img": "./assets/images/lenovo-V14-8-256.jpg"
     },
     {
-     
+        "name": "Lenovo ThinkBook 14\" 16Gb 512Gb",
+        "brand": "Lenovo",
+        "display": "14",
+        "ram": "16",
+        "storage": "512",
+        "color": "grey",
+        "price": "1000",
+        "year": "2021",
+        "img": "./assets/images/lenovo-thinkbook-16-512.jpg"
     },
     {
-      
+        "name": "Dell Vostro 16\" 16Gb 1Tb",
+        "brand": "Dell",
+        "display": "16",
+        "ram": "16",
+        "storage": "1024",
+        "color": "silver",
+        "price": "1590",
+        "year": "2022",
+        "img": "./assets/images/dell-vostro-16-1024.jpg"
     },
     {
-
+        "name": "Dell Latitude 15\" 16Gb 512Gb",
+        "brand": "Dell",
+        "display": "15",
+        "ram": "16",
+        "storage": "512",
+        "color": "black",
+        "price": "1490",
+        "year": "2021",
+        "img": "./assets/images/dell-latitude-16-512.jpg"
     },
     {
-
+        "name": "Dell XPS 14\" 8Gb 512Gb",
+        "brand": "Dell",
+        "display": "14",
+        "ram": "8",
+        "storage": "512",
+        "color": "grey",
+        "price": "1980",
+        "year": "2022",
+        "img": "./assets/images/dell-latitude-16-512.jpg"
     },
+    {
+        "name": "Dell Vostro 13\" 8Gb 256gb",
+        "brand": "Dell",
+        "display": "13",
+        "ram": "8",
+        "storage": "256",
+        "color": "silver",
+        "price": "950",
+        "year": "2020",
+        "img": "./assets/images/dell-vostro-8-256.jpg"
+    }
   ]
+
+  let contentProducts = document.querySelector('.content-products');
 
 // ------------------ Слайд-меню ------------------ //
 
@@ -145,3 +263,127 @@ function openStorage() {
 function openColor() {
     colorBlock.classList.toggle('color-block-open');
 }
+
+// ------------------ Фильтрация ------------------ //
+
+let checkBoxList = document.querySelectorAll('input[type=checkbox]');
+
+for (let el of checkBoxList) {
+    el.addEventListener('click', filterArrProducts);
+}
+
+let newArrProductsResult = [];
+filterArrProducts();
+
+function filterArrProducts() {
+
+    let newArrProductsBrand = [];
+    let newArrProductsDisplay = [];
+    let newArrProductsRam = [];
+    let newArrProductsStorage = [];
+    let newArrProductsColor = [];
+    
+
+    productsArr.forEach(el => {
+        if (checkBoxList[0].checked || checkBoxList[1].checked || checkBoxList[2].checked || checkBoxList[3].checked || checkBoxList[4].checked) {
+            if (el.brand === "Apple" && checkBoxList[0].checked) {
+                newArrProductsBrand.push(el);
+            };
+            if (el.brand === "ASUS" && checkBoxList[1].checked) {
+                newArrProductsBrand.push(el);
+            };
+            if (el.brand === "HP" && checkBoxList[2].checked) {
+                newArrProductsBrand.push(el);
+            };
+            if (el.brand === "Lenovo" && checkBoxList[3].checked) {
+                newArrProductsBrand.push(el);
+            };
+            if (el.brand === "Dell" && checkBoxList[4].checked) {
+                newArrProductsBrand.push(el);
+            };
+        } else {
+            newArrProductsBrand = productsArr;
+        }
+    });
+    newArrProductsBrand.forEach(el=> {
+        if (checkBoxList[5].checked || checkBoxList[6].checked || checkBoxList[7].checked || checkBoxList[8].checked) {
+            if (el.display === "13" && checkBoxList[5].checked) {
+                newArrProductsDisplay.push(el);
+            };
+            if (el.display === "14" && checkBoxList[6].checked) {
+                newArrProductsDisplay.push(el);
+            };
+            if (el.display === "15" && checkBoxList[7].checked) {
+                newArrProductsDisplay.push(el);
+            };
+            if (el.display === "16" && checkBoxList[8].checked) {
+                newArrProductsDisplay.push(el);
+            };
+        } else {
+            newArrProductsDisplay = newArrProductsBrand;
+        }
+    });
+    newArrProductsDisplay.forEach(el=> {
+        if (checkBoxList[9].checked || checkBoxList[10].checked) {
+            if (el.ram === "8" && checkBoxList[9].checked) {
+            newArrProductsRam.push(el);
+            };
+            if (el.ram === "16" && checkBoxList[10].checked) {
+                newArrProductsRam.push(el);
+            };
+        } else {
+            newArrProductsRam = newArrProductsDisplay;
+        }
+    });
+    newArrProductsRam.forEach(el=> {
+        if (checkBoxList[11].checked || checkBoxList[12].checked || checkBoxList[13].checked) {
+            if (el.storage === "256" && checkBoxList[11].checked) {
+                newArrProductsStorage.push(el);
+            };
+            if (el.storage === "512" && checkBoxList[12].checked) {
+                newArrProductsStorage.push(el);
+            };
+            if (el.storage === "1024" && checkBoxList[13].checked) {
+                newArrProductsStorage.push(el);
+            };
+        } else {
+            newArrProductsStorage = newArrProductsRam;
+        }
+    });
+    newArrProductsRam.forEach(el=> {
+        if (checkBoxList[14].checked || checkBoxList[15].checked || checkBoxList[16].checked) {
+            if (el.color === "silver" && checkBoxList[14].checked) {
+                newArrProductsColor.push(el);
+            };
+            if (el.color === "black" && checkBoxList[15].checked) {
+                newArrProductsColor.push(el);
+            };
+            if (el.color === "grey" && checkBoxList[16].checked) {
+                newArrProductsColor.push(el);
+            };
+        } else {
+            newArrProductsColor = newArrProductsStorage;
+        }
+    newArrProductsResult = newArrProductsColor;
+    });
+    console.log(newArrProductsResult);
+    renderProducts();
+}
+
+// ------------------ Рендер списка продуктов ------------------ //
+
+renderProducts();
+
+function renderProducts() {
+    console.log('рендерю!')
+    contentProducts.innerHTML = '';
+    newArrProductsResult.forEach(el => {
+        let tempDiv = document.createElement('div');
+        tempDiv.classList.add('element');
+        tempDiv.innerHTML = `<img src="${el.img}" alt=""><p>${el.name}</p>`
+        contentProducts.appendChild(tempDiv);
+    });
+}
+
+
+
